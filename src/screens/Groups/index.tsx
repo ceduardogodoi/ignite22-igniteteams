4c68854f94d6e7be5a2786501467771bb3a797ad
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-11-29 08:34:45 -0300

    Criando Componente GroupCard

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
@@ -10,6 +11,11 @@ export function Groups() {
       <Highlight
         title="Turmas"
         subtitle="Jogue com a sua turma"
+      />
+
+      <GroupCard
+        title="Galera do Ignite"
+        onPress={() => console.log('pressed')}
       />
     </Container>
   );
