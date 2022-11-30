5398e511fd10290036466f2d89a6c03837e67a25
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-11-30 15:42:38 -0300

    Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,12 +1,13 @@
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 import { Container } from './styles';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   return (
     <Container>
@@ -23,6 +24,12 @@ export function Groups() {
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={!groups.length && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message="Que tal cadastrar a primeira turma?"
           />
         )}
       />
