a557b0b28a482d1907a9f4a9fa7dbf4da27191d1
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2022-12-06 11:30:53 -0300

    Listando os filtros

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
-import { Container, Form } from './styles';
+import { Container, Form, HeaderList, NumberOfPlayers } from './styles';
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState(['Rodrigo', 'Diego', 'Vini', 'Biro']);
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,9 +30,24 @@ export function Players() {
         />
       </Form>
 
-      <Filter
-        title="Time A"
-      />
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+
+        <NumberOfPlayers>
+          {players.length}
+        </NumberOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -17,3 +17,18 @@ export const Form = styled.View`
     background-color: ${theme.COLORS.GRAY_700};
   `};
 `;
+
+export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `};
+`;
